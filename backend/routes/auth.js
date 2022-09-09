e93aed1521755eaf9bb922b76928579f581a7524
Manishjain92744 <manishkumar25031999@gmail.com>
2022-09-10 00:02:27 +0530

    login end point is added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -44,8 +44,44 @@ router.post('/createuser',[
    }
    catch(error){
     console.log(error.message);
-    res.status(500).send("some error occured");
+    res.status(500).send("Internal server error occured");
    }
+})
+
+//Login user using post "/api/auth/createuser" . no login is required 
+router.post('/login',[
+   body('email','Enter a valid email').isEmail(),
+   body('password','Password can not be blank').exists(),
+],async (req, res) => {
+      // if there are error return bad requests and the  errors 
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+     return res.status(400).json({ errors: errors.array() });
+   }
+const { email , password} = req.body;
+try{
+  let user = await User.findOne({email});
+  if(!user){
+   return res.status(400).json({error:"please try to login with correct credentials"});
+  }
+   const passwordCompare = await bcrypt.compare(password,user.password);
+   if(!passwordCompare){
+      return res.status(400).json({error:"please try to login with correct credentials"});
+   }
+
+   const data ={
+      user:{
+         id:user.id
+      }
+    }
+    const authtoken = jwt.sign(data,JWT_SECRET);
+    res.json({authtoken})
+ 
+
+} catch(error){
+    console.log(error.message);
+    res.status(500).send("Internal server Error");
+}
 })
 
 
