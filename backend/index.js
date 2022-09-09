2cca1267f6a2fcce04e240a4e1321a79fa30a834
Manishjain92744 <manishkumar25031999@gmail.com>
2022-09-09 17:42:52 +0530

    new way of authentication is added

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,14 @@ const express = require('express')
 
 connectToMongo();
 const app = express()
-const port = 3000
+const port = 5000
+
+app.use(express.json())
 
 // Available routes
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/auth',require('./routes/notes'))
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,43 @@
 const express=require('express');
+const User = require('../models/User');
 const router=express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.get('/',(req,res)=>{
- obj={
-    a:'thios',
-    number:42
- }
- res.json(obj)
+//create user using post "/api/auth/createuser" . no login is required 
+
+router.post('/createuser',[
+   body('name','Enter a valid name ').isLength({min:4}),
+   body('email','Enter a valid email').isEmail(),
+   body('password','password atleast must be 5 charactter').isLength({ min: 5 }),
+],async (req, res) => {
+   // if there are error return bad requests and the  errors 
+   // Finds the validation errors in this request and wraps them in an object with handy functions
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+     return res.status(400).json({ errors: errors.array() });
+   }
+   try{ 
+   // check whether the user with emial exists already 
+   let user = await User.findOne({email:req.body.email});
+   if(user){
+      return res.status(400).json({error:"sorry already user with this email "})
+   }
+   user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+    
+   //  .then(user => res.json(user))
+   //  .catch(err=>{console.log(err)
+   // res.json({error:'please enter a unique value for email', message:err.message})})
+   res.json(user)
+   }
+   catch(error){
+    console.log(error.message);
+    res.status(500).send("some error occured");
+   }
 })
+
 
 module.exports = router
