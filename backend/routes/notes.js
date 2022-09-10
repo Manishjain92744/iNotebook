f1736d0481c47d106ff0ebabf1b590dfb3041565
Manishjain92744 <manishkumar25031999@gmail.com>
2022-09-10 22:46:05 +0530

    update note end point is added

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -5,7 +5,7 @@ const fetchuser=require('../middleware/fetchuser');
 const { body, validationResult } = require('express-validator');
 
 
-//ROUTE 1:fetch all notes of user  using : GET "/api/auth/fetchallnotes" . no login is required 
+//ROUTE 1:fetch all notes of user  using : GET "/api/notes/fetchallnotes" . no login is required 
 router.get('/fetchallnotes',fetchuser,async (req, res) => {
     try {
       const notes = await Note.find({user:req.user.id});
@@ -17,7 +17,7 @@ router.get('/fetchallnotes',fetchuser,async (req, res) => {
        }
        
 })
-//ROUTE 2:fetch all notes of user  using : GET "/api/auth/addnote" .  login is required 
+//ROUTE 2:fetch all notes of user  using : GET "/api/notes/addnote" .  login is required 
 router.post('/addnote',fetchuser,[
     body('title','Enter name of title  ').isLength({min:4}),
     body('description','write  description of title  ').isLength({ min: 5 }),
@@ -40,5 +40,27 @@ router.post('/addnote',fetchuser,[
         res.status(500).send("Internal server error occured");
        }
      
+})
+
+//ROUTE 3:update notes of user  using : PUT "/api/notes/updatenote" .  login is required 
+router.put('/updatenote/:id',fetchuser,async (req, res) => { 
+  const {title,description,tag}=req.body;
+  //create new note
+  const newNote={};
+  if(title){newNote.title=title};
+  if(description){newNote.description=description};
+  if(tag){newNote.tag=tag};
+
+  let note = await Note.findById(req.params.id);
+  if(!note){return res.status(404).send("not found")}
+
+  if(note.user.toString() !== req.user.id)
+  {
+    return res.status(401).send("not allowed")
+  }
+
+  note = await Note.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true})
+  res.json({note})
+  
 })
 module.exports = router
